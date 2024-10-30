5ff7e36cee5b7c465ff10ba025dcc0c92d55d349
Chira2 <a20170043@juan23.edu.pe>
2024-10-29 23:22:24 -0500

    Initial commit

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1> Bienvenido a la pagina de Java script</h1>");
+var nom = prompt("ingrese su nombre");
+document.write(
+  "hola " + nom + " Es tu primer contacto con la programación de java script"
+);
